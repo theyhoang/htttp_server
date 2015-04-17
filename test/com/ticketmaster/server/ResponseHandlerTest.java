d32c703f4659a3d6869699547522bb6898ad0fd9
Yen Hoang <theyhoang@gmail.com>
2015-04-17 14:40:50 -0700

    unit test should be only testing method of class

--- test/com/ticketmaster/server/ResponseHandlerTest.java
+++ test/com/ticketmaster/server/ResponseHandlerTest.java
@@ -1,5 +1,6 @@
 package com.ticketmaster.server;
 
+import com.ticketmaster.server.model.Method;
 import com.ticketmaster.server.model.Request;
 import com.ticketmaster.server.model.Response;
 import org.junit.Assert;
@@ -18,28 +19,16 @@ import java.util.List;
 public class ResponseHandlerTest {
     @Test
     public void testConstructResponse() {
-        String testRequestString = "GET / HTTP/1.1\r\n";
-        testRequestString += "Host: localhost:9090\r\n";
-        testRequestString += "Connection: keep-alive\r\n";
-        testRequestString += "Accept: */*\r\n";
-        testRequestString += "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36\r\n";
-        testRequestString += "Accept-Encoding: gzip, deflate, sdch\r\n";
-        testRequestString += "Accept-Language: en-US,en;q=0.8\r\n";
-        testRequestString += "\r\n";
-
 
         DataOutputStream out = new DataOutputStream(System.out);
-        InputStream stream = new ByteArrayInputStream(testRequestString.getBytes(StandardCharsets.UTF_8));
         PrintWriter printWriter = new PrintWriter(out, true);
         ResponseHandler responseHandler = new ResponseHandler(printWriter);
         responseHandler.setPublicDirPath("");
-        RequestHandler requestHandler = new RequestHandler(new BufferedReader( new InputStreamReader(stream)));
-        Request request = null;
-        try {
-            request = requestHandler.readRequest();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        Request request = new Request();
+        request.setHttpVersion("HTTP/1.1");
+        request.setHttpMethod(Method.GET.name());
+        request.setUrl("/");
 
         Assert.assertNotNull(request);
         Response response = responseHandler.getResponse(request);
